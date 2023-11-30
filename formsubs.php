ba619d3e97770764083032dc6de96a3a2323a576
JHayes278 <150937903+JHayes278@users.noreply.github.com>
2023-11-29 20:16:28 -0500

    Full

---
+++ formsubs.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<div>
+<div>
+            Welcome <?php echo $_POST["name"]; ?><br>
+            Thank you for your comment: <?php echo $_POST["comment"]; ?>
+</div>
+</div>
+</body>
+</html>
